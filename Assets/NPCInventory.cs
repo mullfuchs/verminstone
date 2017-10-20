ae2bdd906e283809a807a2095aecbe4a69912749
Leo Burke <burke.leo@gmail.com>
2017-10-20 00:20:54 -0700

    Adding equippable items and item prefabs

--- Assets/NPCInventory.cs
+++ Assets/NPCInventory.cs
@@ -3,21 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NPCInventory : MonoBehaviour {
+    
+    public GameObject ObjectHeldInHands;
+    public GameObject ObjectOnBack;
 
-    public List<GameObject> _NPCInventory;
-
-    public GameObject LeftHand;
-    public GameObject RightHand;
-    public GameObject Back;
+    private Transform handTransform;
+    private Transform backTransform;
+    private GameObject ActiveHandObject;
+    private GameObject ActiveBackObject;
 
 	// Use this for initialization
 	void Start () {
-        _NPCInventory = new List<GameObject>();
+        handTransform = FindDeepChild(gameObject.transform, "palm_r");
+        backTransform = FindDeepChild(gameObject.transform, "neck");
+        if(ObjectHeldInHands != null)
+        {
+            ActiveHandObject = Instantiate(ObjectHeldInHands, handTransform.position, handTransform.rotation, handTransform);
+        }
+        if (ObjectOnBack != null)
+        {
+            ActiveBackObject = Instantiate(ObjectOnBack, backTransform.position, backTransform.rotation, backTransform);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void Equip(GameObject GearObject)
+    {
+
+    }
     
+    Transform getBodyTransform(string part)
+    {
+        Transform hand;
+        hand = transform.Find(part);
+        if(hand == null)
+        {
+            print("couldn't find part " + part);
+            return gameObject.transform;
+        }
+        else
+        {
+            print("found part");
+            return hand;
+        }
+    }
+
+    void EquipItem(GameObject item, GameObject slot, Transform bodyPart)
+    {
+        if(slot != null)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public Transform FindDeepChild(Transform aParent, string aName)
+    {
+        var result = aParent.Find(aName);
+        if (result != null)
+            return result;
+        foreach (Transform child in aParent)
+        {
+            result = FindDeepChild(child, aName);
+            if (result != null)
+                return result;
+        }
+        return null;
+    }
+
 }
