f3e3d7d127353d5bc7a3c92bb1ef67ea21f797fc
Leo Burke <burke.leo@gmail.com>
2017-11-04 00:22:12 -0700

    minor art and cosmetic changes

--- Assets/Standard Assets/Scripts/LookAtTargetAndWalkForward.cs
+++ Assets/Standard Assets/Scripts/LookAtTargetAndWalkForward.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class LookAtTargetAndWalkForward : MonoBehaviour {
 	public float walkSpeed;
-	public GameObject target;	
-	// Update is called once per frame
-	void Start (){
-		if (Random.Range (0, 2) <= 0 && target == null) {
+	public GameObject target;
+
+    private GameObject AttackHitBox;
+    private bool CanAttack = true;
+    // Update is called once per frame
+    void Start (){
+        AttackHitBox = transform.Find("AttackHitBox").gameObject;
+        AttackHitBox.SetActive(false);
+
+        if (Random.Range (0, 2) <= 0 && target == null) {
 			target = GameObject.FindGameObjectWithTag ("Player");
 		} 
 		else {
@@ -17,16 +23,37 @@ public class LookAtTargetAndWalkForward : MonoBehaviour {
 	}
 
 	void Update () {
-//		if (target != null) {
-//			//transform.LookAt (target.transform.position);
-//		} else {
-//			//target = GameObject.FindGameObjectWithTag ("Player");
-//		}
-//		transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+        if(target != null)
+        {
+            float distToEnemy = Vector3.Distance(target.transform.position, transform.position);
+            if (distToEnemy <= 1.5f && CanAttack)
+            {
+                PerformAttack();
+            }
+        }
 	}
 
     public void setTarget(GameObject newTarget)
     {
         this.gameObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControl>().target = newTarget.transform;
     }
+
+    public void PerformAttack()
+    {
+        AttackHitBox.SetActive(true);
+        CanAttack = false;
+        Invoke("HideHitBox", 0.5f);
+        Invoke("ResetAttack", 1.5f);
+    }
+
+    public void HideHitBox()
+    {
+        AttackHitBox.SetActive(false);
+    }
+
+    public void ResetAttack()
+    {
+        CanAttack = true;
+    }
+
 }
