a47d33d928959db270fcbf3896c1a2a5cabf337c
mullfuchs <burke.leo@gmail.com>
2018-10-20 18:48:06 -0700

    Fixed NPC attacking phantom bugs somehow

---
+++ Assets/Editor/SelectByTag.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class SelectByTag : MonoBehaviour {
+
+	private static string SelectedTag = "Bug";
+
+	[MenuItem("Helpers/Select By Tag")]
+	public static void SelectObjectsWithTag()
+	{
+		GameObject[] objects = GameObject.FindGameObjectsWithTag(SelectedTag);
+		Selection.objects = objects;
+	}
+}
---
+++ Assets/WeaponController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+	public int damage;
+
+	// Use this for initialization
+	void Start () {
+		//get parent
+
+		//find 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void UpdateNPCHitboxDamage(){
+		Transform hitbox = gameObject.transform.parent.Find("AttackHitBox");
+
+		if (hitbox != null) {
+			hitbox.GetComponent<DealDamageToObjects> ().increaseHitDamage (damage);
+		} else {
+			print ("can't find hitbox");
+		}
+		hitbox.gameObject.SetActive (false);
+	}
+}
