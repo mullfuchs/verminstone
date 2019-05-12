e581002ad93f74663ebfddc76119eb8cf67e7b4a
mullfuchs <burke.leo@gmail.com>
2019-05-12 12:54:23 -0700

    adding 2 more enemy types, including new geo, prefabs, and spawner hookups

--- Assets/EnemyTeamHandler.cs
+++ Assets/EnemyTeamHandler.cs
@@ -12,7 +12,7 @@ public class EnemyTeamHandler : MonoBehaviour {
 
     public GameObject SwarmEnemy;
     public GameObject MediumEnemy;
-    public GameObject HeavyPatrolEnemy;
+    public GameObject BossEnemy;
 
 	public int floorLevel = 1;
 
@@ -44,6 +44,16 @@ public class EnemyTeamHandler : MonoBehaviour {
 
 		closestsSpawner.GetComponent<SpawnObjects>().SpawnEnemy(SwarmEnemy, MinerList.ToArray(), floorLevel, floorLevel);
 		randSpawner.GetComponent<SpawnObjects> ().SpawnEnemy (SwarmEnemy, MinerList.ToArray (), floorLevel, floorLevel);
+
+		//one out of like..10 chance to spawn a medium
+		if (Random.Range (1, 10) <= 1) {
+			closestsSpawner.GetComponent<SpawnObjects>().SpawnEnemy(MediumEnemy, MinerList.ToArray(), floorLevel, floorLevel);
+		}
+		//one out of like 20 to spawn a boss
+		if (Random.Range (1, 20) <= 1) {
+			closestsSpawner.GetComponent<SpawnObjects>().SpawnEnemy(BossEnemy, MinerList.ToArray(), floorLevel, floorLevel);
+		}
+
 
 
     }
