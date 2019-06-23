e67426bd2d211c028d077aa3b5a4467e7a9a72fb
mullfuchs <burke.leo@gmail.com>
2019-06-23 15:30:32 -0700

    tutorial stuff

---
+++ Assets/ExitTutorialLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitTutorialLevel : MonoBehaviour {
+
+	public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter( Collider Other ){
+		if (Other.gameObject.name == "Player") {
+			destroyPlayerAndNPCS ();
+			SceneManager.LoadScene (levelToLoad);
+		}
+	}
+
+	void destroyPlayerAndNPCS(){
+		GameObject[] npcs = GameObject.FindGameObjectsWithTag ("WorkerNPC");
+		for (int i = 0; i < npcs.Length; i++) {
+			Destroy (npcs [i]);
+		}
+		Destroy(GameObject.Find("Player"));
+	}
+}
