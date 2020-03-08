a3cd6e2f86334e7e3d4e1615ef8e26148d5091e8
Leo Burke <burke.leo@gmail.com>
2020-03-07 23:02:27 -0800

    escape sequence stuff

---
+++ Assets/SetDialogNodeForNPCOnEnter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetDialogNodeForNPCOnEnter : MonoBehaviour {
+    public string NodeToSet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "WorkerNPC")
+        {
+            other.gameObject.GetComponent<Yarn.Unity.Example.NPC>().talkToNode = NodeToSet;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/SetTargetForNPCOnEnter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTargetForNPCOnEnter : MonoBehaviour {
+
+    public GameObject target;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "WorkerNPC")
+        {
+            other.GetComponent<AIStateMachine>().AddTargetForNPC(target);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
