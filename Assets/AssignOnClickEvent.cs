f0cfc0372f77842af40bdfe5f32e493a9d3498ae
Leo Burke <burke.leo@gmail.com>
2021-04-26 22:29:43 -0700

    bugfixes

---
+++ Assets/AssignOnClickEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AssignOnClickEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(Button_OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Button_OnClick()
+    {
+        GameObject.Find("CampEventController").GetComponent<CampEventController>().EnterCaveSequence();
+    }
+
+}
